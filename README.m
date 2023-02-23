ec2096371c2eaba49fe9fd6e2406cb92072bab30
Gerald LeCour <84100086+Gerald-LeCour@users.noreply.github.com>
2023-02-23 14:12:34 -0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,5 @@
+# Ellen G White Tribute Page
+
+Hello, I wanted to make a tribute page to someone I admire, that being Ellen G White who is one of the founders of the Seventh Day Adventist church and also has a gift of prophecy. 
+I tryed learning how to code over a year ago but then i stopped for a long time, but now I wanted to come back and try to freshen up my skills that I used to have.
+It took me a while just to make this but I am proud of myself for making this. The only thing this is missing is that its not mobile friendly, it... it looks garbage on mobile.
